d8bf548ba752db551d041ace7e4144f193ac22d1
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2025-02-28 08:44:04 +0100

    fixed ParallaxScrollView.tsx: added default padding to children, optional header height, onScrollOffsetChange, dynamicStyles using StyleSheet, borderRadius, etc for great UI and UX

--- altaviz_mobile/components/ParallaxScrollView.tsx
+++ altaviz_mobile/components/ParallaxScrollView.tsx
@@ -5,6 +5,7 @@ import Animated, {
   useAnimatedRef,
   useAnimatedStyle,
   useScrollViewOffset,
+  runOnJS,
 } from 'react-native-reanimated';
 
 import { ThemedView } from '@/components/ThemedView';
@@ -12,33 +13,52 @@ import { useBottomTabOverflow } from '@/components/ui/TabBarBackground';
 import { useColorScheme } from '@/hooks/useColorScheme';
 
 const HEADER_HEIGHT = 250;
+const DEFAULT_CHILDREN_PADDING = 32;
 
 type Props = PropsWithChildren<{
   headerImage: ReactElement;
   headerBackgroundColor: { dark: string; light: string };
+  headerHeight?: number;
+  childrenPadding?: number;
+  onScrollOffsetChange?: any;
 }>;
 
 export default function ParallaxScrollView({
   children,
   headerImage,
   headerBackgroundColor,
+  headerHeight = HEADER_HEIGHT,
+  childrenPadding = DEFAULT_CHILDREN_PADDING,
+  onScrollOffsetChange
 }: Props) {
   const colorScheme = useColorScheme() ?? 'light';
   const scrollRef = useAnimatedRef<Animated.ScrollView>();
   const scrollOffset = useScrollViewOffset(scrollRef);
   const bottom = useBottomTabOverflow();
+  const dynamicStyles = StyleSheet.create({
+    height: {
+      height: headerHeight,
+    },
+    childrenPadding: {
+      padding: childrenPadding
+    }
+  });
   const headerAnimatedStyle = useAnimatedStyle(() => {
+    const offset = scrollOffset.value;
+    // console.log({offset});
+    if (onScrollOffsetChange)
+      runOnJS(onScrollOffsetChange)(offset);
     return {
       transform: [
         {
           translateY: interpolate(
-            scrollOffset.value,
-            [-HEADER_HEIGHT, 0, HEADER_HEIGHT],
-            [-HEADER_HEIGHT / 2, 0, HEADER_HEIGHT * 0.75]
+            offset,
+            [-headerHeight, 0, headerHeight],
+            [-headerHeight / 2, 0, headerHeight * 0.75]
           ),
         },
         {
-          scale: interpolate(scrollOffset.value, [-HEADER_HEIGHT, 0, HEADER_HEIGHT], [2, 1, 1]),
+          scale: interpolate(offset, [-headerHeight, 0, headerHeight], [2, 1, 1]),
         },
       ],
     };
@@ -53,13 +73,13 @@ export default function ParallaxScrollView({
         contentContainerStyle={{ paddingBottom: bottom }}>
         <Animated.View
           style={[
-            styles.header,
+            styles.header, dynamicStyles.height,
             { backgroundColor: headerBackgroundColor[colorScheme] },
             headerAnimatedStyle,
           ]}>
           {headerImage}
         </Animated.View>
-        <ThemedView style={styles.content}>{children}</ThemedView>
+        <ThemedView style={[styles.content,dynamicStyles.childrenPadding]}>{children}</ThemedView>
       </Animated.ScrollView>
     </ThemedView>
   );
@@ -70,13 +90,14 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   header: {
-    height: HEADER_HEIGHT,
+    padding: 15,
     overflow: 'hidden',
+    borderRadius: 10,
   },
   content: {
     flex: 1,
-    padding: 32,
-    gap: 16,
+    // gap: 16,
+    // paddingTop: 20,
     overflow: 'hidden',
   },
 });
