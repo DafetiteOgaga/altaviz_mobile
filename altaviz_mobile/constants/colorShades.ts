f54b5a3115dfe850be80edcc34666b9a169d4d01
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2025-02-28 08:46:35 +0100

    colorShades.ts

---
+++ altaviz_mobile/constants/colorShades.ts
@@ -0,0 +1,44 @@
+// {
+// 	"Blue Shades": [
+// 	  { "name": "Near Black Blue", "hex": "#0A0D1A" },
+// 	  { "name": "Very Dark Blue", "hex": "#121A33" },
+// 	  { "name": "Darker Blue", "hex": "#1A2750" },
+// 	  { "name": "Dark Blue", "hex": "#223370" },
+// 	  { "name": "Base Blue", "hex": "#283593" },
+// 	  { "name": "Lighter Blue", "hex": "#4C5EA3" },
+// 	  { "name": "Even Lighter Blue", "hex": "#7689C8" },
+// 	  { "name": "Very Light Blue", "hex": "#A1B3E5" },
+// 	  { "name": "Near White Blue", "hex": "#D5DBF2" },
+// 	  { "name": "Almost White Blue", "hex": "#F0F3FC" }
+// 	],
+// 	"Gray Shades": [
+// 	  { "name": "Near Black Gray", "hex": "#1A1A1A" },
+// 	  { "name": "Very Dark Gray", "hex": "#333333" },
+// 	  { "name": "Darker Gray", "hex": "#4D4D4D" },
+// 	  { "name": "Dark Gray", "hex": "#666666" },
+// 	  { "name": "Base Gray", "hex": "#A6A6A6" },
+// 	  { "name": "Lighter Gray", "hex": "#BFBFBF" },
+// 	  { "name": "Even Lighter Gray", "hex": "#D9D9D9" },
+// 	  { "name": "Very Light Gray", "hex": "#EAEAEA" },
+// 	  { "name": "Near White Gray", "hex": "#F5F5F5" },
+// 	  { "name": "Almost White Gray", "hex": "#FAFAFA" }
+// 	],
+// 	"Black Shades": [
+// 	  { "name": "True Black", "hex": "#000000" },
+// 	  { "name": "Almost Black", "hex": "#0D0D0D" },
+// 	  { "name": "Very Dark Black", "hex": "#141414" },
+// 	  { "name": "Dark Black", "hex": "#1B1B1B" },
+// 	  { "name": "Base Black", "hex": "#212121" },
+// 	  { "name": "Charcoal Black", "hex": "#333333" },
+// 	  { "name": "Slate Black", "hex": "#4D4D4D" },
+// 	  { "name": "Dull Black", "hex": "#666666" },
+// 	  { "name": "Pale Black", "hex": "#808080" },
+// 	  { "name": "Near Gray Black", "hex": "#999999" }
+// 	],
+// 	"White Shades": [
+// 	  { "name": "Very Light Grayish White", "hex": "#F5F5F5" },
+// 	  { "name": "Light Grayish White", "hex": "#FAFAFA" },
+// 	  { "name": "Pure White", "hex": "#FFFFFF" }
+// 	]
+//   }
+  
