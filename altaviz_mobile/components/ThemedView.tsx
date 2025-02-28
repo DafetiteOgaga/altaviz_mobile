be4aa908a35a12531f0eb572f93ece202b31ec6b
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2025-02-28 08:45:28 +0100

    fixed ThemedView.tsx: switched from dark/light themes to blue them (consistent color)

--- altaviz_mobile/components/ThemedView.tsx
+++ altaviz_mobile/components/ThemedView.tsx
@@ -1,6 +1,6 @@
 import { View, type ViewProps } from 'react-native';
 
-import { useThemeColor } from '@/hooks/useThemeColor';
+import { useColorMode } from '@/constants/Colors';
 
 export type ThemedViewProps = ViewProps & {
   lightColor?: string;
@@ -8,7 +8,6 @@ export type ThemedViewProps = ViewProps & {
 };
 
 export function ThemedView({ style, lightColor, darkColor, ...otherProps }: ThemedViewProps) {
-  const backgroundColor = useThemeColor({ light: lightColor, dark: darkColor }, 'background');
-
-  return <View style={[{ backgroundColor }, style]} {...otherProps} />;
+  const uniColorMode = useColorMode();
+  return <View style={[{ backgroundColor: uniColorMode.vvvdrkbltr }, style]} {...otherProps} />;
 }
