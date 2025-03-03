f4e629de57212fae8c765d0f5f09a5cbef43ff9a
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2025-03-04 00:48:02 +0100

    fixed pendingFaults.tsx: added suppport for workshop, help desk and supervisor

--- altaviz_mobile/app/(tabs)/pendingFaults.tsx
+++ altaviz_mobile/app/(tabs)/pendingFaults.tsx
@@ -7,8 +7,11 @@ import { useGet } from '@/requests/makeRequests';
 import { useColorMode } from '@/constants/Colors';
 import { useGetIcon } from '@/components/getIcon';
 import { CardView } from '@/components/cardView';
+import { EngineerCardView } from '@/components/engineersCardView';
+import { useGetDataFromStorage } from '@/context/useGetDataFromStorage';
 
 export default function PendingFaults() {
+	const userDetails = useGetDataFromStorage('headerDetails')
 	const [refreshing, setRefreshing] = useState<boolean>(false);
 	const uniColorMode = useColorMode();
 	const {getData, isGetError, isGetLoading, GetSetup} = useGet();
@@ -18,7 +21,7 @@ export default function PendingFaults() {
 	useEffect(()=>{GetSetup(url)}, [url])
 	let name
 	const nameList = String(label).split(' ')
-	if (String(label).split(' ').length===3) {name = nameList[1]}
+	if (String(label).split(' ').length===3||label==='Posted Parts') {name = nameList[1]}
 	else {name = 'Fault'}
 	const handleRefresh = () => { // Refresh/pull new data from server upon refresh
         setRefreshing(true);
@@ -26,7 +29,10 @@ export default function PendingFaults() {
         setRefreshing(false);
     }
 	const {getIcon, color} = useGetIcon({variant: String(variant)})
-	useEffect(()=>setHeaderTitle(String(label)), [isGetLoading])
+	useEffect(()=>setHeaderTitle(String(label)), [isGetLoading, url, label, variant])
+	const role = userDetails?.role
+	console.log('in pendingFaults', {role})
+	const supervisor_HelpDesk_HR = role==='help-desk'||role==='supervisor'
 	return (
 		<>
 			{(!isGetLoading&&getData)?
@@ -35,20 +41,25 @@ export default function PendingFaults() {
 					<FlatList
 					data={getData}
 					keyExtractor={(item: Record<string, any>) => item.id.toString()}
-					renderItem={({ item }) => (
-						<TouchableOpacity
-						activeOpacity={0.8}
-						onPress={() => navigation.navigate('detailScreen', {
-							data: JSON.stringify(item),
-							arrayData: JSON.stringify(getData),
-							type: name,
-							variant: variant
-						})}
-						>
-							<CardView mode={String(mode)} icon={getIcon!} color={color!} item={item} />
-
-						</TouchableOpacity>
-					)}
+					renderItem={({ item }) => {
+						const user = item?.first_name
+						console.log('pendingFaults:', {user})
+						return (
+							<TouchableOpacity
+							activeOpacity={0.8}
+							onPress={() => navigation.navigate(supervisor_HelpDesk_HR?'engineersFaults':'detailScreen', {
+								data: JSON.stringify(item),
+								arrayData: JSON.stringify(getData),
+								type: name,
+								variant: variant,
+								user: user,
+							})}
+							>{supervisor_HelpDesk_HR?
+								<EngineerCardView mode={String(mode)} icon={getIcon!} color={color!} item={item} role={role}/>
+								:
+								<CardView mode={String(mode)} icon={getIcon!} color={color!} item={item} role={role}/>}
+							</TouchableOpacity>
+						)}}
 					ItemSeparatorComponent={()=><View style={{height: 6}} />}
 					ListEmptyComponent={()=><Text style={[generalstyles.notFound, {color: uniColorMode.text,}]}>{'All Good! 💪\nNothing to Display'}</Text>}
 					refreshControl={
