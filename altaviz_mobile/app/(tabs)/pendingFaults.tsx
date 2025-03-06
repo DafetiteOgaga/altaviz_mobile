3e036428b208c904f29a9cc880ec337f78be5f82
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2025-03-06 14:34:04 +0100

    fixed pendingFaults.tsx: added an extra layer view to PendingFaults(), just after rendering the list to improve UI

--- altaviz_mobile/app/(tabs)/pendingFaults.tsx
+++ altaviz_mobile/app/(tabs)/pendingFaults.tsx
@@ -1,4 +1,5 @@
-import { View, Text, StyleSheet, ActivityIndicator, FlatList, TouchableOpacity, RefreshControl } from 'react-native';
+import { View, Text, StyleSheet, ActivityIndicator, FlatList, TouchableOpacity,
+	RefreshControl, Modal } from 'react-native';
 import React, {useEffect, useState} from 'react';
 import { useLocalSearchParams, useRouter } from 'expo-router';
 import { ScreenStyle, generalstyles } from '../../myConfig/navigation';
@@ -6,94 +7,119 @@ import { useHeader } from '@/context/headerUpdate';
 import { useGet } from '@/requests/makeRequests';
 import { useColorMode } from '@/constants/Colors';
 import { useGetIcon } from '@/components/getIcon';
-import { CardView } from '@/components/cardView';
+import { CardView } from '../../components/cardView';
 import { EngineerCardView } from '@/components/engineersCardView';
-import { DetailsRequestCardView } from '@/components/detailsRequestCardView';
+import { DetailsRequestCardView } from '../../components/detailsRequestCardView';
 import { useGetDataFromStorage } from '@/context/useGetDataFromStorage';
+import { getComponentName } from '@/hooks/getComponentName';
 
 export default function PendingFaults() {
+	getComponentName()
 	const userDetails = useGetDataFromStorage('headerDetails')
 	const [refreshing, setRefreshing] = useState<boolean>(false);
 	const uniColorMode = useColorMode();
+	const [isNavigating, setIsNavigating] = useState<boolean>(false);
+	const [modalItemVisible, setModalItemVisible] = useState<any>(false);
 	const {getData, isGetError, isGetLoading, GetSetup} = useGet();
 	
 	const {url, label, mode, variant} = useLocalSearchParams();
-	console.log(
-		'\nin pendingFaults #######:',
-		// '\ndata:', JSON.stringify(userData, null, 4),
-        '\n', {url},
-        '\n', {label},
-        '\n', {mode},
-        '\n', {variant},
-	)
+	// console.log(
+	// 	'\nin pendingFaults #######:',
+	// 	// '\ndata:', JSON.stringify(userData, null, 4),
+    //     '\n', {url},
+    //     '\n', {label},
+    //     '\n', {mode},
+    //     '\n', {variant},
+	// )
 	const router = useRouter()
 	useEffect(()=>{GetSetup(url)}, [url])
 	let name
 	const nameList = String(label).split(' ')
 	if (String(label).split(' ').length===3||label==='Posted Parts') {name = nameList[1]}
 	else {name = 'Fault'}
-	const handleRefresh = () => { // Refresh/pull new data from server upon refresh
+	const handleRefresh = async () => { // Refresh/pull new data from server upon refresh
         setRefreshing(true);
-        GetSetup(url)
+        await GetSetup(url)
         setRefreshing(false);
     }
 	const {getIcon, color} = useGetIcon({variant: String(variant)})
 	// const { setHeaderTitle } = useHeader();
 	// useEffect(()=>setHeaderTitle(String(label)), [isGetLoading, url, label, variant])
 	const role = userDetails?.role
-	console.log('in pendingFaults', {role})
+	// console.log('in pendingFaults', {role})
 	const acountUpdate = (role==='human-resource'&&label==='Account Update Requests')||null
-	console.log('in pendingFaults', {role})
+	// console.log('in pendingFaults', {role})
 	const HRExceptuion = String(url)?.split?.('-')?.[3]?.split?.('/')?.[0]
 	const supervisor_HelpDesk_HR = role==='help-desk'||role==='supervisor'||(role==='human-resource'&&mode==='fault'&&!HRExceptuion)
 	// console.log('pendingFaults (first item):', JSON.stringify(getData?.[0], null, 4))
 	const allHRRequests = String(url)?.split?.('/')[0] === 'all-request-only'
+	const navigationHandler = (item:any, user:any) => {
+		if (isNavigating) return
+		setTimeout(() => setIsNavigating(true), 10);
+		// setIsNavigating(true)
+		try {
+			router.push({
+				// @ts-ignore
+				pathname: acountUpdate?'/userChangeRequest':supervisor_HelpDesk_HR?'/engineersFaults':'/detailScreen',
+				params: {
+				data: JSON.stringify(item),
+				arrayData: JSON.stringify(getData),
+				type: name,
+				variant: variant,
+				user: user,
+				label: String(label),
+			}})
+			setTimeout(() => setIsNavigating(false), 1000);
+		} catch (error) {
+			console.log('error:', error)
+		}
+	}
 	return (
 		<>
 			{(!isGetLoading&&getData)?
 				(<>
-				<View style={[ScreenStyle.allScreenContainer, styles.listContainer]}>
-					<FlatList
-					data={getData}
-					keyExtractor={(item: Record<string, any>) => item.id.toString()}
-					renderItem={({ item }) => {
-						const user = acountUpdate?userDetails?.id:item?.first_name
-						console.log('pendingFaults:', {user})
-						return (
-							<TouchableOpacity
-							activeOpacity={0.8}
-							onPress={() => router.push({
-								// @ts-ignore
-								pathname: acountUpdate?'/userChangeRequest':supervisor_HelpDesk_HR?'/engineersFaults':'/detailScreen',
-								params: {
-								data: JSON.stringify(item),
-								arrayData: JSON.stringify(getData),
-								type: name,
-								variant: variant,
-								user: user,
-								label: String(label),
-							}})}
-							>{acountUpdate?
-								<DetailsRequestCardView mode={String(mode)} icon={getIcon!} color={color!} item={item} role={role} label={String(label)}/>
-								:
-								supervisor_HelpDesk_HR?
-									<EngineerCardView mode={String(mode)} icon={getIcon!} color={color!} item={item} role={role} label={String(label)}/>
+					{isNavigating && (
+						<Modal visible={isNavigating} animationType="fade" transparent>
+							<View style={styles.overlay}>
+								<ActivityIndicator size="large" color={uniColorMode.buttonSpin} />
+							</View>
+						</Modal>
+					)}
+
+					<View style={[ScreenStyle.allScreenContainer, styles.listContainer]}>
+						<FlatList
+						data={getData}
+						keyExtractor={(item: Record<string, any>) => item.id.toString()}
+						renderItem={({ item }) => {
+							const user = acountUpdate?userDetails?.id:item?.first_name
+							// console.log('pendingFaults:', {user})
+							return (
+								<TouchableOpacity
+								activeOpacity={0.8}
+								onPress={()=>{navigationHandler(item, user)}}
+								>{acountUpdate?
+									<DetailsRequestCardView mode={String(mode)} icon={getIcon!} color={color!} item={item} role={role} label={String(label)}/>
 									:
-									<CardView mode={String(mode)} icon={getIcon!} color={color!} item={item} role={role} label={String(label)} swapCard={allHRRequests}/>}
-							</TouchableOpacity>
-						)}}
-					ItemSeparatorComponent={()=><View style={{height: 6}} />}
-					ListEmptyComponent={()=><Text style={[generalstyles.notFound, {color: uniColorMode.text,}]}>{'All Good! 💪\nNothing to Display'}</Text>}
-					refreshControl={
-						<RefreshControl
-						refreshing={refreshing}
-						onRefresh={handleRefresh}
-						colors={[uniColorMode.mb]}
-						tintColor={uniColorMode.mb} // iOS spinner color
+									supervisor_HelpDesk_HR?
+										<EngineerCardView mode={String(mode)} icon={getIcon!} color={color!} item={item} role={role} label={String(label)}/>
+										:
+										<CardView mode={String(mode)} icon={getIcon!} color={color!} item={item} role={role} label={String(label)} swapCard={allHRRequests}/>}
+								</TouchableOpacity>
+							)}}
+						ItemSeparatorComponent={()=><View style={{height: 6}} />}
+						ListEmptyComponent={()=><Text style={[generalstyles.notFound, {color: uniColorMode.text,}]}>{'All Good! 💪\nNothing to Display'}</Text>}
+						refreshControl={
+							<RefreshControl
+							refreshing={refreshing}
+							onRefresh={handleRefresh}
+							colors={[uniColorMode.mb]}
+							tintColor={uniColorMode.mb} // iOS spinner color
+							/>
+						}
 						/>
-					}
-					/>
-				</View></>) :
+					</View>
+				</>)
+				:
 				<ActivityIndicator style={styles.loading} size="large" color={uniColorMode.buttonSpin} />
 			}
 		</>
@@ -108,4 +134,34 @@ const styles = StyleSheet.create({
 	loading: {
 		marginTop: 250,
 	},
+	overlay: {
+		position: "absolute",
+		top: 0,
+		left: 0,
+		right: 0,
+		bottom: 0,
+		justifyContent: "center",
+		alignItems: "center",
+		backgroundColor: "rgba(0,0,0,0.5)", // Semi-transparent background
+		zIndex: 1000,
+	},
 })
+
+
+// {isNavigating && (
+// 	<View style={styles.overlay}>
+// 		<ActivityIndicator size="large" color={uniColorMode.buttonSpin} />
+// 	</View>
+// )}
+
+// overlay: {
+// 	position: "absolute",
+// 	top: 0,
+// 	left: 0,
+// 	right: 0,
+// 	bottom: 0,
+// 	justifyContent: "center",
+// 	alignItems: "center",
+// 	backgroundColor: "rgba(0,0,0,0.5)", // Semi-transparent background
+// 	zIndex: 1000,
+// },
