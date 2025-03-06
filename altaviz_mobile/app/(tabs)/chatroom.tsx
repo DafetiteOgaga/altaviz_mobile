f9ee263f882b614d8c4a3067fd1b9cd962fb2711
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2025-03-06 14:20:05 +0100

    moved chatroom.tsx: moved from  parent dir into this

---
+++ altaviz_mobile/app/(tabs)/chatroom.tsx
@@ -0,0 +1,32 @@
+import { Link, Stack } from 'expo-router';
+import { StyleSheet, View } from 'react-native';
+import React from 'react';
+import { ThemedText } from '../../components/ThemedText';
+import { ThemedView } from '../../components/ThemedView';
+import { ScreenStyle } from '../../myConfig/navigation';
+import { getComponentName } from '../../hooks/getComponentName';
+
+export default function ChatRoom() {
+  getComponentName()
+  return (
+    <>
+      <Stack.Screen />
+      <ThemedView style={[ScreenStyle.allScreenContainer, styles.container]}>
+        <ThemedText type="title">This is the chatroom page.</ThemedText>
+        {/* <ThemedText type="link">Go nowhere!</ThemedText> */}
+      </ThemedView>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  link: {
+    // marginTop: 15,
+    paddingVertical: 15,
+  },
+});
