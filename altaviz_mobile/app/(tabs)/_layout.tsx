9688020baa551d00405c26ec09ae4e533d857b0c
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2025-03-13 14:18:16 +0100

    fixed _layout.tsx: removed commented codes

--- altaviz_mobile/app/(tabs)/_layout.tsx
+++ altaviz_mobile/app/(tabs)/_layout.tsx
@@ -2,9 +2,6 @@ import { Tabs } from 'expo-router';
 import React from 'react';
 import { Platform } from 'react-native';
 import { HapticTab } from '@/components/HapticTab';
-// import TabBarBackground from '@/components/ui/TabBarBackground';
-// import { Colors } from '@/constants/Colors';
-// import { useColorScheme } from '@/hooks/useColorScheme';
 import { screenConfig } from '@/myConfig/navigation';
 import { Ionicons } from '@expo/vector-icons';
 import { useColorMode } from '@/constants/Colors';
@@ -42,53 +39,13 @@ export default function TabLayout() {
                 tabBarIcon: ({ color }) => <Ionicons size={28} name={screenConfig.index.icon} color={color} />,
             }}
             />
-            <Tabs.Screen // dashboard screen
-            name="appIndex" // screen name (navigation)
-            // initialParams={{ title: 'Explore Tab' }}
-            options={{
-                href: null, // Hides tab but keeps screen accessible via navigation.navigate()
-                // title: screenConfig.dashboard.title, // tab title
-                // tab icon and color
-                tabBarIcon: ({ color }) => <Ionicons size={28} name={screenConfig.appIndex.icon} color={color} />,
-                tabBarBadge: 3, // show a badge on the tab
-                tabBarBadgeStyle: { // badge style
-                backgroundColor: uniColorMode.vltb,
-                },
-            }}
-            />
-            <Tabs.Screen // explore screen
-            name="explore" // screen name (navigation)
-            // initialParams={{ title: 'Explore Tab' }}
-            options={{
-                href: null, // Hides tab but keeps screen accessible via navigation.navigate()
-                // title: screenConfig.explore.title, // tab title
-                // tab icon and color
-                tabBarIcon: ({ color }) => <Ionicons size={28} name={screenConfig.explore.icon} color={color} />,
-                tabBarBadge: 3, // show a badge on the tab
-                tabBarBadgeStyle: { // badge style
-                backgroundColor: '#1111e1',
-                },
-            }}
-            />
-            <Tabs.Screen // greet screen
-            name="greet" // screen name (navigation)
-            initialParams={{ // default parameter value
-                message: 'Hello, Anon!\nFron default parameter value'
-            }}
-            options={{
-                href: null, // Hides tab but keeps screen accessible via navigation.navigate()
-                // title: screenConfig.greet.title, // tab title
-                // tab icon and color
-                tabBarIcon: ({ color }) => <Ionicons size={28} name={screenConfig.greet.icon} color={color} />,
-            }}
-            />
             <Tabs.Screen // chatroom screen
             name="chatroom" // screen name (navigation)
             initialParams={{ // default parameter value
                 message: 'Hello, Anon!\nFron default parameter value'
             }}
             options={{
-                // title: screenConfig.greet.title, // tab title
+                // title: screenConfig.chatroom.title, // tab title
                 // tab icon and color
                 tabBarIcon: ({ color }) => <Ionicons size={28} name={screenConfig.chatroom.icon} color={color} />,
                 tabBarBadge: 3, // show a badge on the tab
@@ -103,7 +60,7 @@ export default function TabLayout() {
                 message: 'Hello, Anon!\nFron default parameter value'
             }}
             options={{
-                // title: screenConfig.greet.title, // tab title
+                // title: screenConfig.profile.title, // tab title
                 // tab icon and color
                 tabBarIcon: ({ color }) => <Ionicons size={28} name={screenConfig.profile.icon} color={color} />,
             }}
