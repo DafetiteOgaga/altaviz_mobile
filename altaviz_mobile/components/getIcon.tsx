5a34292897ec825f7011e205a75d4999683525a2
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2025-03-18 22:26:13 +0100

    fixed getIcon.tsx: added conditions for account, allFaults and allRequests

--- altaviz_mobile/components/getIcon.tsx
+++ altaviz_mobile/components/getIcon.tsx
@@ -7,6 +7,9 @@ export function useGetIcon({variant}: {variant: string}) {
 	else if (variant === "pendingParts") {color = '#007BFF'; getIcon = 'cube-outline'}
 	else if (variant === "faults") {color = '#FF4C4C'; getIcon = 'construct-outline'}
 	else if (variant === "unconfirmedResolutions") {color = '#FFA500'; getIcon = 'hourglass-outline'}
+	else if (variant === "account") {color = '#D3D3D3'; getIcon = 'construct-outline'}
+	else if (variant === "allFaults") {color = '#FFD700'; getIcon = 'hourglass-outline'}
+	else if (variant === "allRequests") {color = '#4682B4'; getIcon = 'hourglass-outline'}
 	// console.log({variant}, {color}, {getIcon})
 	return {color, getIcon}
 }
